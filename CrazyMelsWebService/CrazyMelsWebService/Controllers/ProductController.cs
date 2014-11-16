d1ea87ece460ed34f69e1a41a95e0d34373226ff
Verdi R-D <verdi@azend.org>
2014-11-16 16:31:02 -0500

    Added deletion to products

--- CrazyMelsWebService/CrazyMelsWebService/Controllers/ProductController.cs
+++ CrazyMelsWebService/CrazyMelsWebService/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace CrazyMelsWebService.Controllers
 {
@@ -26,10 +27,35 @@ namespace CrazyMelsWebService.Controllers
 
             return data.ToArray();
 
+        }
 
+        // DELETE api/Order/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            C_Product c_product = db.C_Product.Find(id);
+            if (c_product == null)
+            {
+                return NotFound();
+            }
 
+            db.C_Product.Remove(c_product);
 
+            foreach (C_Cart cart in db.C_Cart.Where(c => c.prodID == id))
+            {
+                db.C_Cart.Remove(cart);
+            }
 
+            db.SaveChanges();
+
+            Product p = new Product();
+            p.prodID = c_product.prodID;
+            p.prodName = c_product.prodName;
+            p.prodWeight = c_product.prodWeight;
+            p.price = p.price;
+            p.inStock = c_product.inStock;
+
+            return Ok(p);
         }
     }
 }
