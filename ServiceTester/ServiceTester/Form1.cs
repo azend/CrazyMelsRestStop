b34ef2b83d31554eaa02bacdffe04aeffd32b156
Verdi R-D <verdi@azend.org>
2014-11-16 15:17:13 -0500

    Added tester for testing the rest service

---
+++ ServiceTester/ServiceTester/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            string body = textBox2.Text;
+
+            string response = await RunAsync(url, body);
+            textBox3.Text = response;
+        }
+
+        private static async Task<string> RunAsync(string url, string body)
+        {
+            string res = String.Empty;
+
+            using (var client = new HttpClient())
+            {
+                // TODO - Send HTTP requests
+
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/customer/");
+                if (response.IsSuccessStatusCode)
+                {
+                    res = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return res;
+        }
+    }
+}
