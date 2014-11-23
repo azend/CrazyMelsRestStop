2aa7dfbc7eb15abfaa036db0ff5120b9fabf7a20
Sean J <game_freak_9937@hotmail.com>
2014-11-22 21:09:15 -0500

    fixed customer validation

--- CrazyMelsClient/CrazyMelsClient/Screen2.cs
+++ CrazyMelsClient/CrazyMelsClient/Screen2.cs
@@ -557,10 +557,37 @@ namespace CrazyMelsClient
                         {
                             if (!ValidateIDOrQuantity(customerCustID_textbox.Text))
                             {
+                                return;
+                            }
+                            else
+                            {
                                 firstName_textbox.Text = "";
                                 lastName_textbox.Text = "";
                                 phoneNumber_textbox.Text = "";
-                                return;
+                            }
+                        }
+                        else
+                        {
+                            if (firstName_textbox.Text != "")
+                            {
+                                if (!ValidateCustomerName(firstName_textbox.Text, 1))
+                                {
+                                    return;
+                                }
+                            }
+                            if (lastName_textbox.Text != "")
+                            {
+                                if (!ValidateCustomerName(lastName_textbox.Text, 2))
+                                {
+                                    return;
+                                }
+                            }
+                            if (phoneNumber_textbox.Text != "")
+                            {
+                                if (!ValidateCustomerPhoneNumber())
+                                {
+                                    return;
+                                }
                             }
                         }
                     }                    
