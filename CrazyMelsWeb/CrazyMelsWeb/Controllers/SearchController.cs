d70dc67439ff55a54acdebaeb58a755e544412a7
Verdi R-D <verdi@azend.org>
2014-11-24 08:19:27 -0500

    Added first semi-working search :D

--- CrazyMelsWeb/CrazyMelsWeb/Controllers/SearchController.cs
+++ CrazyMelsWeb/CrazyMelsWeb/Controllers/SearchController.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Web.Http;
 
 namespace CrazyMelsWeb.Controllers
@@ -12,74 +14,84 @@ namespace CrazyMelsWeb.Controllers
     {
         private CrazyMelsEntities db = new CrazyMelsEntities();
 
-        public IEnumerable<CrazyMelDataModel> Get()
+        private static Expression<Func<TItem, bool>> PropertyEquals<TItem, TValue>(
+    PropertyInfo property, TValue value)
+        {
+            var param = Expression.Parameter(typeof(TItem));
+            var body = Expression.Equal(Expression.Call(Expression.Property(param, property), typeof(object).GetMethod("ToString")),
+                Expression.Constant(value));
+            return Expression.Lambda<Func<TItem, bool>>(body, param);
+        }
+
+        [Route("api/search/{*sParameters}")]
+        public IEnumerable<CrazyMelDataModel> Get(string sParameters)
         {
             List<CrazyMelDataModel> data = new List<CrazyMelDataModel>();
 
-            //TODO: add some sort of request query
-            /*
-            IQueryable<Cart> carts = from cart in db.C_Cart
-                                     //where
-                                     select new Cart
-                                     {
-                                         orderID = cart.orderID,
-                                         prodID = cart.prodID,
-                                         quantity = cart.quantity
-                                     };
+            if (!String.IsNullOrWhiteSpace(sParameters))
+            {
 
-            IQueryable<Product> products = from product in db.C_Product
-                                     //where
-                                     select new Product(product);
+                List<Customer> customers = new List<Customer>();
+                List<Cart> carts = new List<Cart>();
+                List<Order> orders = new List<Order>();
+                List<Product> products = new List<Product>();
 
-            IQueryable<Order> orders = from order in db.C_Order
-                                     //where
-                                     select new Order(order);
+                IList<string> parameters = sParameters.Split('/');
+                
 
-            IQueryable<Customer> customers = from customer in db.C_Customer
-                                     //where
-                                     select new Customer(customer);
-             * */
+                foreach (string param in parameters)
+                {
+                    IEnumerable<string> kvp = param.Split('=');
 
-            /*
-            data.AddRange(carts.ToArray());s
-            data.AddRange(products.ToArray());
-            data.AddRange(orders.ToArray());
-            data.AddRange(customers.ToArray());
-             * */
+                    if (!string.IsNullOrWhiteSpace(kvp.First()) && !string.IsNullOrWhiteSpace(kvp.Last()))
+                    {
+                        IEnumerable<string> propertyLocatorPieces = kvp.First().Split('.');
+                        if (propertyLocatorPieces.Count() == 2)
+                        {
+                            string className = propertyLocatorPieces.First();
+                            string propertyName = propertyLocatorPieces.Last();
+                            PropertyInfo propertyInfo = null;
+                            string value = kvp.Last();
 
-            data.AddRange( db.C_Cart
-                .Where(c => true)
-                .ToList()
-                .Select(c => new Cart { orderID = c.orderID, prodID = c.prodID, quantity = c.quantity })
-            );
+                            switch (className)
+                            {
+                                case "Customer":
+                                    propertyInfo = typeof(C_Customer).GetProperty(propertyName);
+                                    customers.AddRange(db.C_Customer.Where(PropertyEquals<C_Customer, string>(propertyInfo, value)).Select(c => new Customer { custID = c.custID, firstName = c.firstName, lastName = c.lastName, phoneNumber = c.phoneNumber }));
+                                    break;
+                                case "Cart":
+                                    propertyInfo = typeof(C_Cart).GetProperty(propertyName);
+                                    carts.AddRange(db.C_Cart.Where(PropertyEquals<C_Cart, string>(propertyInfo, value)).Select(c => new Cart { orderID = c.orderID, prodID = c.prodID, quantity = c.quantity }));
+                                    break;
+                                case "Order":
+                                    propertyInfo = typeof(C_Order).GetProperty(propertyName);
+                                    orders.AddRange(db.C_Order.Where(PropertyEquals<C_Order, string>(propertyInfo, value)).Select(c => new Order { custID = c.custID, orderDate = c.orderDate, orderID = c.orderID, poNumber = c.poNumber }));
+                                    break;
+                                case "Product":
+                                    propertyInfo = typeof(C_Product).GetProperty(propertyName);
+                                    products.AddRange(db.C_Product.Where(PropertyEquals<C_Product, string>(propertyInfo, value)).Select(c => new Product { prodID = c.prodID, prodName = c.prodName, prodWeight = c.prodWeight, price = c.price, inStock = c.inStock }));
+                                    break;
+                            }
+                        }
 
-            data.AddRange(db.C_Product
-                .Where(p => true)
-                .ToList()
-                .Select(p => new Product { prodID = p.prodID, price = p.price, prodName = p.prodName, prodWeight = p.prodWeight, inStock = p.inStock})
-            );
 
-            data.AddRange(db.C_Order
-                .Where(o => true)
-                .ToList()
-                .Select(o => new Order { orderID = o.orderID, orderDate = o.orderDate, custID = o.custID, poNumber = o.poNumber })
-            );
+                    }
+                }
 
-            data.AddRange(db.C_Customer
-                .Where(c => true)
-                .ToList()
-                .Select(c => new Customer { custID = c.custID, firstName = c.firstName, lastName = c.lastName, phoneNumber = c.phoneNumber })
-            );
+                data.AddRange(customers);
+                data.AddRange(carts);
+                data.AddRange(orders);
+                data.AddRange(products);
+            }
 
-            /*
-            data.AddRange(db.C_Cart.Where(c => true).ToList());
-            data.AddRange(db.C_Customer.Where(c => true).ToList());
-            data.AddRange(db.C_Order.Where(c => true).ToList());
-            data.AddRange(db.C_Product.Where(c => true).ToList());
-             */
+            
+
+            
 
             return data.ToArray();
 
         }
+
+       
     }
 }
