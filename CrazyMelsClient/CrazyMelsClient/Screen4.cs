11106f2f51b716ec3ccd5be7ed3b5a65e85fbf87
MathewCain <goaliesupreme@hotmail.com>
2014-11-24 08:18:06 -0500

    Screen 4 outputs to tables

--- CrazyMelsClient/CrazyMelsClient/Screen4.cs
+++ CrazyMelsClient/CrazyMelsClient/Screen4.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CrazyMelsWeb.Models;
 
 namespace CrazyMelsClient
 {
@@ -50,6 +49,9 @@ namespace CrazyMelsClient
 
         public void customerList(List<Customer> customer)
         {
+            DataGridView customerGridView = new DataGridView();
+            int x = 0;
+
             string[] names = { "Customer ID", "First Name", "Last Name", "Phone Number" };
 
             if (customer.Count == 0)
@@ -57,10 +59,23 @@ namespace CrazyMelsClient
                 return;
             }
 
-            generateTable("customerGridView", 4, names);
+            generateTable("customerGridView", 4, names, customerGridView);
+
+            foreach (Customer cust in customer)
+            {
+                customerGridView.Rows.Add(1);
+                customerGridView.Rows[x].Cells[0].Value = cust.custID.ToString();
+                customerGridView.Rows[x].Cells[1].Value = cust.firstName.ToString();
+                customerGridView.Rows[x].Cells[2].Value = cust.lastName.ToString();
+                customerGridView.Rows[x].Cells[3].Value = cust.phoneNumber.ToString();
+                x++;
+            }
         }
         public void productList(List<Product> product)
         {
+            DataGridView productGridView = new DataGridView();
+            int x = 0;
+
             string[] names = { "Product ID", "Product Name", "Price", "Product Weight", "In Stock?" };
 
             if (product.Count == 0)
@@ -68,10 +83,24 @@ namespace CrazyMelsClient
                 return;
             }
 
-            generateTable("productGridView", 5, names);
+            generateTable("productGridView", 5, names, productGridView);
+
+            foreach (Product prod in product)
+            {
+                productGridView.Rows.Add(1);
+                productGridView.Rows[x].Cells[0].Value = prod.prodID.ToString();
+                productGridView.Rows[x].Cells[1].Value = prod.prodName.ToString();
+                productGridView.Rows[x].Cells[2].Value = prod.price.ToString();
+                productGridView.Rows[x].Cells[3].Value = prod.prodWeight.ToString();
+                productGridView.Rows[x].Cells[4].Value = prod.inStock.ToString();
+                x++;
+            }
         }
         public void orderList(List<Order> order)
         {
+            DataGridView orderGridView = new DataGridView();
+            int x = 0;
+
             string[] names = { "Order ID", "Customer ID", "PO Number", "Order Date" };
 
             if (order.Count == 0)
@@ -79,23 +108,45 @@ namespace CrazyMelsClient
                 return;
             }
 
-            generateTable("orderGridView", 4, names);
+            generateTable("orderGridView", 4, names, orderGridView);
+
+            foreach (Order ord in order)
+            {
+                orderGridView.Rows.Add(1);
+                orderGridView.Rows[x].Cells[0].Value = ord.orderID.ToString();
+                orderGridView.Rows[x].Cells[1].Value = ord.custID.ToString();
+                orderGridView.Rows[x].Cells[2].Value = ord.poNumber.ToString();
+                orderGridView.Rows[x].Cells[3].Value = ord.orderDate.ToString();
+                x++;
+            }
         }
         public void cartList(List<Cart> cart)
         {
+            DataGridView cartGridView = new DataGridView();
+            int x = 0;
+
             string[] names = { "Order ID", "Product ID", "Quantity" };
 
             if (cart.Count == 0)
             {
                 return;
             }
-            
-            generateTable("cartGridView", 3, names);
+
+            generateTable("cartGridView", 3, names, cartGridView);
+
+            foreach (Cart car in cart)
+            {
+                cartGridView.Rows.Add(1);
+                cartGridView.Rows[x].Cells[0].Value = car.orderID.ToString();
+                cartGridView.Rows[x].Cells[1].Value = car.prodID.ToString();
+                cartGridView.Rows[x].Cells[2].Value = car.quantity.ToString();
+                x++;
+            }
         }
 
-        private void generateTable(string name, int columnNumber, string[] columnNames)
+        private void generateTable(string name, int columnNumber, string[] columnNames, DataGridView dataGridView1)
         {
-            DataGridView dataGridView1 = new DataGridView();
+           // DataGridView dataGridView1 = new DataGridView();
             this.Controls.Add(dataGridView1);
 
             dataGridView1.Name = name;
